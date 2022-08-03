fa7ded3e91666cd86f7ef192648750b0d75d8ba0
Leandro Magnotti <leandro.magnotti@gmail.com>
2022-08-02 23:56:19 -0300

    Agregando calculadora...

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,15 @@
-function calcular() {
-    let primerNro = parseInt(prompt("Ingresa primer Nro"))
-    let segundoNro = parseInt(prompt("Ingresa el segundo Nro"))
-    let operador = prompt("Selecciona operacion {+, -, *, /}")
+let neuroParcial1 = 2400 
+let neuroParcial2 = 2400
+let neuroFinal = 4000
+
+function calcularTotal() {
+    debugger
+    let opcionParcial1 = prompt("Queres acceder al curso para el Primer Parcial de Neuro? (Si - No)")
+    let opcionParcial2 = prompt("Queres acceder al curso para el Segundo Parcial de Neuro? (Si - No)")
+    let opcionFinal = prompt("Queres acceder al curso para el Final de Neuro? (Si - No)")
     console.log("Resultado", realizarCalculo(primerNro, segundoNro, operador))
 }
-function realizarCalculo(num1, num2, ope) switch(ope) {
-    case "+":
-        return num1 + num2
-    case "-":
-        return num1 - num2
-    case "*":
-        return num1 * num2
-    case "/":
-        return num1 / num2
-    default:
-        return "Error, sos flor de puto" 
-}
+function realizarCalculo(num1, num2, ope) {switch(ope) {
+    case "si", "Si", "SI":
+        return neuroParcial1 
+}}
