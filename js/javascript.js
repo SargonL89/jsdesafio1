54f7c7ffd484c8ad40a418da2660a3f8edb2c7a5
Leandro Magnotti <leandro.magnotti@gmail.com>
2022-08-07 23:20:49 -0300

    Cambios según after class

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,26 @@
+
+function ingresarNombre() {
+    debugger
+    let nombre = prompt("Ingrese su nombre");
+    while (nombre === "" || nombre === null) {
+    nombre = prompt("Ingrese su nombre")};
+    console.log("Bienvenido a la sección Cursos", nombre,", esperamos que estés teniendo un buen día")
+}
+
+function demandaYRespuesta() {
+    debugger
+    let opcionParcial1 = prompt("Si querés acceder al curso para el Primer Parcial de Neuro, presioná 1, sino, presioná 2")
+    let opcionParcial2 = prompt("Si querés acceder al curso para el Segundo Parcial de Neuro, presioná 1, sino, presioná 2")
+    let opcionFinal = prompt("Si querés acceder al curso para el Final de Neuro, presioná 1, sino, presioná 2");
+    console.log("Perfecto! el total a abonar es: $", calculadora(opcionParcial1, opcionParcial2, opcionFinal))
+}
+
 let neuroParcial1 = 2400 
 let neuroParcial2 = 2400
 let neuroFinal = 4000
 
-function calcularTotal() {
-    debugger
-    let opcionParcial1 = prompt("Queres acceder al curso para el Primer Parcial de Neuro? (Si - No)")
-    let opcionParcial2 = prompt("Queres acceder al curso para el Segundo Parcial de Neuro? (Si - No)")
-    let opcionFinal = prompt("Queres acceder al curso para el Final de Neuro? (Si - No)")
-    console.log("Resultado", realizarCalculo(primerNro, segundoNro, operador))
+function calculadora(curso1, curso2, curso3) {
+    if (curso1 === "1", curso2 === "2", curso3 === "2") {return neuroParcial1}
+    else if (curso1 === "1", curso2 === "1", curso3 === "2") {return neuroParcial1 + neuroParcial2}
+    else if (curso1 === "1", curso2 === "1", curso3 === "1") {return neuroParcial1 + neuroParcial2 + neuroFinal;}
 }
-function realizarCalculo(num1, num2, ope) {switch(ope) {
-    case "si", "Si", "SI":
-        return neuroParcial1 
-}}
